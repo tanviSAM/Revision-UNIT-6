5dd025a3fca9a08a28bad2a4ff140be53bb4102d
Tanvi Samant <96070177+tanviSAM@users.noreply.github.com>
2022-06-20 18:20:33 +0530

    Completed

--- twitter schema/src/index.js
+++ twitter schema/src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.listen(3000, async () => {
   try {
     await connect();
-    console.log("listening on port 5000");
+    console.log("listening on port 3000");
   } catch (err) {
     console.log("Error:", err);
   }
